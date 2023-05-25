c00a0a92c3e6a4e4725eda4019e366d6ccdd860f
Piyush Dwivedi <121784263+aarmin9696@users.noreply.github.com>
2023-05-25 13:02:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,42 @@
 function calculateMinCost() {
-  //your code here
-	let lengths = getElementById("rope-lengths")
-	 const ropes = lengths.split(",").map(Number);
+  const inputElement = document.getElementById("rope-lengths");
+  const resultElement = document.getElementById("result");
+  
+  // Get the comma-separated rope lengths from the input
+  const input = inputElement.value;
+  const ropeLengths = input.split(",").map(length => parseInt(length.trim()));
+  
+  // Calculate the minimum cost of connecting ropes
+  const minCost = getMinimumCost(ropeLengths);
+  
+  // Display the result in the <div id="result"></div> element
+  resultElement.innerText = "Minimum cost: " + minCost;
+}
 
-  ropes.sort((a, b) => a - b); // Sort the ropes in ascending order
-
-  let cost = 0;
-
-  while (ropes.length > 1) {
-    const rope1 = ropes.shift(); // Remove the smallest rope from the front
-    const rope2 = ropes.shift(); // Remove the second smallest rope from the front
-
-    const newRope = rope1 + rope2;
-    cost += newRope;
-
-    ropes.push(newRope); // Add the new rope to the end of the array
-    ropes.sort((a, b) => a - b); // Sort the ropes again
+function getMinimumCost(ropeLengths) {
+  // Sort the rope lengths in ascending order
+  ropeLengths.sort((a, b) => a - b);
+  
+  let totalCost = 0;
+  
+  while (ropeLengths.length > 1) {
+    // Take the two smallest ropes
+    const smallest1 = ropeLengths.shift();
+    const smallest2 = ropeLengths.shift();
+    
+    // Connect the ropes and add the cost to the total
+    const cost = smallest1 + smallest2;
+    totalCost += cost;
+    
+    // Insert the connected rope back into the sorted array
+    const insertIndex = ropeLengths.findIndex(length => length >= cost);
+    if (insertIndex === -1) {
+      // If the connected rope has the largest length, append it at the end
+      ropeLengths.push(cost);
+    } else {
+      ropeLengths.splice(insertIndex, 0, cost);
+    }
   }
-
-  return cost;
-
   
-  
-  
-}  
+  return totalCost;
+}
